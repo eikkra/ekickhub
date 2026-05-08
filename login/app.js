3ad36938f4bc906a7360f7c21c0d5ceead2769ad
MD Rashedul Hasan <mdrashedulhasan02@gmail.com>
2026-05-08 17:23:37 +0600

    Add files via upload

--- login/app.js
+++ login/app.js
@@ -1,53 +1,158 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-const provider = new GoogleAuthProvider()
+import {
+
+getAuth,
+
+GoogleAuthProvider,
+
+signInWithPopup,
+
+signInWithEmailAndPassword
+
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+
+getFirestore,
+
+doc,
+
+getDoc
+
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyDwPilelp6BgHhHD8Hs_cHx96ZJNZdeYag",
+
+authDomain: "ekickhub-bd.firebaseapp.com",
+
+projectId: "ekickhub-bd",
+
+storageBucket: "ekickhub-bd.firebasestorage.app",
+
+messagingSenderId: "306381500871",
+
+appId: "1:306381500871:web:50e1cc59d823872328e9e2"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+
+const db = getFirestore(app)
+
+
+
+window.login = async function(){
+
+const msg =
+document.getElementById("msg")
+
+msg.innerHTML = "Please wait..."
+
+
+const email =
+document.getElementById("email").value
+
+const password =
+document.getElementById("password").value
+
+
+try{
 
 const result =
-await signInWithPopup(auth,provider)
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+)
 
 const user = result.user
 
 
-const docRef = doc(db,"users",user.uid)
+const docRef =
+doc(db,"users",user.uid)
 
-const docSnap = await getDoc(docRef)
+const docSnap =
+await getDoc(docRef)
 
 
 if(!docSnap.exists()){
 
-window.location.href = "../register/"
+msg.innerHTML =
+"Account data not found"
 
 return
 
 }
 
 
-const userData = docSnap.data()
+const userData =
+docSnap.data()
 
 
 if(userData.approved !== true){
 
-msg.innerHTML = "Waiting for admin approval"
+msg.innerHTML =
+"Waiting for admin approval"
 
 return
 
 }
 
 
-if(userData.roles.includes("admin")){
-
-window.location.href = "../admin/"
-
-}else{
-
-window.location.href = "../dashboard/"
-
-}
+msg.innerHTML =
+"Login successful"
 
 
 }catch(error){
 
-msg.innerHTML = error.message
+msg.innerHTML =
+"Invalid email or password"
+
+}
+
+}
+
+
+
+window.googleLogin = async function(){
+
+const msg =
+document.getElementById("msg")
+
+msg.innerHTML =
+"Please wait..."
+
+
+try{
+
+const provider =
+new GoogleAuthProvider()
+
+await signInWithPopup(
+auth,
+provider
+)
+
+window.location.href =
+"../register/"
+
+
+}catch(error){
+
+msg.innerHTML =
+error.message
 
 }
 
